1a519d69569e7c6618c718d1852ccf3cd4f1ade5
Madusha Kumarasiri <madusha.kumarasiri@silvercreeksoftware.com>
2024-01-20 15:53:09 -0700

    Restructured project

---
+++ assets/lego_data.js
@@ -0,0 +1,79 @@
+var link_ebay = "https://www.ebay.com/sch/i.html?_nkw=";
+var minifigs = {
+  "Film Noir Detective": {
+    name: "Film Noir Detective",
+    image: "images/detective.webp"
+  },
+  "E-Sports Gamer": {
+    name: "E-Sports Gamer",
+    image: "images/gamer.webp"
+  },
+  "Vampire Knight": {
+    name: "Vampire Knight",
+    image: "images/vampire.webp"
+  },
+  "Sprinter": {
+    name: "Sprinter",
+    image: "images/sprinter.webp"
+  },
+  "Goatherd": {
+    name: "Goatherd",
+    image: "images/goatherd.webp"
+  },
+  "Mushroom Sprite": {
+    name: "Mushroom Sprite",
+    image: "images/mushroom.webp"
+  },
+  "Fitness Instructor": {
+    name: "Fitness Instructor",
+    image: "images/fitness.webp"
+  },
+  "Triceratops Costume Fan": {
+    name: "Triceratops Costume Fan",
+    image: "images/triceratops.webp"
+  },
+  "Harpy": {
+    name: "Harpy",
+    image: "images/harpy.webp"
+  },
+  "Train Kid": {
+    name: "Train Kid",
+    image: "images/train.webp"
+  },
+  "Fierce Barbarian": {
+    name: "Fierce Barbarian",
+    image: "images/barbarian.webp"
+  },
+  "Pet Groomer": {
+    name: "Pet Groomer",
+    image: "images/petgroomer.webp"
+  },
+}
+
+var map = {
+  "6471965": "Film Noir Detective",
+  "6471969": "E-Sports Gamer",
+  "6471970": "Vampire Knight",
+  "6471966": "Sprinter",
+  "6471962": "Goatherd",
+  "6471961": "Mushroom Sprite",
+  "6471960": "Fitness Instructor",
+  "6471968": "Triceratops Costume Fan",
+  "6471963": "Harpy",
+  "6471964": "Train Kid",
+  "6471959": "Fierce Barbarian",
+  "6471967": "Pet Groomer",
+
+  "6472866": "Film Noir Detective",
+  "6472870": "E-Sports Gamer",
+  "6472871": "Vampire Knight",
+  "6472867": "Sprinter",
+  "6472863": "Goatherd",
+  "6472862": "Mushroom Sprite",
+  "6472861": "Fitness Instructor",
+  "6472869": "Triceratops Costume Fan",
+  "6472864": "Harpy",
+  "6472865": "Train Kid",
+  "6472860": "Fierce Barbarian",
+  "6472868": "Pet Groomer"
+}
